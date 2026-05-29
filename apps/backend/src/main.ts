7144313f1616a7b78133822e0b30711ce8d6bcd3
aosebih <ao.sebih@esi-sba.dz>
2026-05-29 19:21:13 +0100

    fix: dynamic port for Render, add render.yaml

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.enableCors();
-  await app.listen(3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
   console.log('🚀 Server running on http://localhost:3000/api/v1');
 }
 bootstrap();
